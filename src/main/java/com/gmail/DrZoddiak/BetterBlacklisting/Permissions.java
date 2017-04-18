8dc279efcb069711db76cac421198168ede3689d
Bruce Linington <DrZoddiak@gmail.com>
2017-04-18 16:54:22 -0500

    Create Permissions.java
    Initial Creation | Needs work

---
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/Permissions.java
@@ -0,0 +1,8 @@
+package io.drzoddiak.betterblacklisting;
+
+#Banned item list
+#Bypass banned items
+#Remove banned items
+#Add banned items
+#ItemID in hand
+#Edit existing banned items (for reason or types)
