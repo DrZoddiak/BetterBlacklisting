7a9327f026677e5ce6a90183875d3cfc28ffb54a
Bruce Linington <DrZoddiak@gmail.com>
2017-04-18 20:02:24 -0500

    Create Set.java

---
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/Commands/Set.java
@@ -0,0 +1,13 @@
+package io.drzoddiak.betterblacklisting.Commands;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.format.TextColors;
+
+#Idealy the command structure should resemble /bbl set (itemID) true
+#and/or resemble /bbl set true | This of course detecting the item in hand.
