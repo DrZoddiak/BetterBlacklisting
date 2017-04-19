8fb456ff6d122e0724640907231c4b8870fcdd00
Bruce Linington <DrZoddiak@gmail.com>
2017-04-19 06:10:11 -0500

    Create WhatsThis.java

---
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/Commands/WhatsThis.java
@@ -0,0 +1,29 @@
+package com.gmail.DrZoddiak.BetterBlacklisting.Commands;
+
+import java.util.Optional;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.DataQuery;
+import org.spongepowered.api.data.type.HandTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+
+
+public class WhatsThis implements CommandExecutor
+{
+
+	@Override
+	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
