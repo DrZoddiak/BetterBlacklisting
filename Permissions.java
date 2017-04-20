f2dc156279c1623c81d005e767e9f1f8083cc741
Bruce Linington <DrZoddiak@gmail.com>
2017-04-20 12:59:26 -0500

    Small optimizations, added Permissions file

---
+++ Permissions.java
@@ -0,0 +1,8 @@
+package com.gmail.DrZoddiak.BetterBlacklisting;
+
+public class Permissions {
+
+    public final static String Delete_Item = "bbl.admin.delete";
+    public final static String Add_Item = "bbl.admin.add";
+
+}
