547fd1a3928326e9e8bd9387167942adf2c79228
burpingdog1 <bdogsproduction@gmail.com>
2017-04-24 21:40:34 -0500

    try on events?

--- src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/EventHandler.java
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/EventHandler.java
@@ -1,6 +1,9 @@
 package com.gmail.DrZoddiak.BetterBlacklisting;
 
+import java.util.Optional;
+
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.crafting.CraftingOutput;
 import org.spongepowered.api.item.inventory.entity.PlayerInventory;
 
@@ -9,13 +12,20 @@ public class EventHandler
 //Hi there, I am as useful as a screen door on a submarine right now! but I do indeed promise to be useful soon...
 	@Listener
 	public void onCraft(CraftingOutput output)
-	{  
+	{   
 		//Want to craft a banned item? too bad!
 	}
 	
-	@Listener
-	public void invCheck(PlayerInventory inv)
-	{  
+	@Listener()
+		public void invCheck(PlayerInventory inv)
+	{    
+		while(inv.slots().iterator().hasNext())
+		{
+			Optional<ItemStack> temp = inv.slots().iterator().next().peek();
+			if(Main.list.getList().contains(temp.toString()))
+				inv.iterator().remove();
+		}
+			
 		//Want to store a banned item? too bad!
 	}
 }
