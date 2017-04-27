1739b95541690d0267e7d282ae5f279b48b2bc91
burpingdog1 <bdogsproduction@gmail.com>
2017-04-26 19:28:51 -0500

    Try and pick me up now!
    Trying to pick up banned items wont work now... doesn't support metadata
    yet

--- src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/EventListener.java
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/EventListener.java
@@ -3,30 +3,27 @@ package com.gmail.DrZoddiak.BetterBlacklisting;
 import java.util.Iterator;
 import java.util.Optional;
 
+import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.item.inventory.ChangeInventoryEvent;
+import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 
 public class EventListener 
 {
 //Hi there, I am as useful as a screen door on a submarine right now! but I do indeed promise to be useful soon... }
-	
-	@Listener()
-		public void invCheck(ChangeInventoryEvent event)
-	{  
-		Iterator<Inventory> ite = event.getTargetInventory().slots().iterator();
-		while (ite.hasNext())
+	 
+		@Listener()
+		public void onPickUp(ChangeInventoryEvent.Pickup event)
 		{
-			Optional<ItemStack> test = ite.next().peek();
-			if(test.isPresent())
+			ItemType item = event.getTargetEntity().getItemType();
+			if(Main.list.getList().contains(item.getId()))
 			{
-				String item = test.get().getItem().getName(); 
-				System.out.println(item);
-				if(Main.list.getList().contains(item))   
-					System.out.println("banned");
- 			} 
+				event.setCancelled(true);
+				event.getTargetEntity().remove(); 
+				System.out.println(item.getId());
+			}
 		}
 		//Want to store a banned item? too bad!
-	} 
 }
