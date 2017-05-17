ea5ed9abee5581b486187fe374c4f6cf0b34db78
Bruce Linington <DrZoddiak@gmail.com>
2017-05-17 04:02:22 -0500

    Attempt to add functionality to vanilla blockdata
    Doesn't quite work, springs an error.
    It now generates its own jar file

--- src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/Commands/Identify.java
+++ src/main/java/com/gmail/DrZoddiak/BetterBlacklisting/Commands/Identify.java
@@ -3,53 +3,54 @@ package com.gmail.DrZoddiak.BetterBlacklisting.Commands;
 
 import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.command.CommandException;
-
 import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.manipulator.mutable.item.DurabilityData;
-
-import org.spongepowered.api.data.value.BaseValue;
 import org.spongepowered.api.data.type.HandTypes;
-import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
-import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
-import org.spongepowered.api.text.channel.MessageChannel;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.data.value.mutable.Value;
+import net.minecraft.item.ItemBlock;
 
+@SuppressWarnings("ALL")
 public class Identify implements CommandExecutor
 {
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException
     {
+        Player player = (Player) src;
+        String identity = "";
 
-        if(src instanceof Player)
+        if (!player.getItemInHand(HandTypes.MAIN_HAND).isPresent())
         {
-            Player player = (Player) src;
-            if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent())
-            {
-                ItemStack itemInHand =  player.getItemInHand(HandTypes.MAIN_HAND).get();
 
-                String Identity = itemInHand.getItem().getId();
-                if(itemInHand.supports(Keys.ITEM_BLOCKSTATE))
-                player.sendMessage(Text.of(Identity));
+            player.sendMessage(Text.of("There is nothing in your hand!"));
 
-                if(!itemInHand.supports(Keys.ITEM_BLOCKSTATE))
-                    player.sendMessage(Text.of(Identity + ":" + itemInHand.get(Keys.ITEM_BLOCKSTATE)));
-
-
-            }
-            else
-                src.sendMessage(Text.of(TextColors.RED, "Your Main hand has nothing in it!"));
+            return CommandResult.success();
         }
         else
-            throw new CommandException(Text.of("You cannot use this command from console!"));
+        {
+            ItemStack itemInHand =  player.getItemInHand(HandTypes.MAIN_HAND).get();
+
+            if (itemInHand.getItem() instanceof ItemBlock)
+            {
+                ItemBlock itemBlock = (ItemBlock) itemInHand.getItem();
+                net.minecraft.item.ItemStack nmsStack = (net.minecraft.item.ItemStack) (Object) itemInHand;
+                BlockState blockState = ((BlockState) itemBlock.getBlock().getStateFromMeta(nmsStack.getItemDamage()));
+                identity = blockState.getId();
+
+
+                player.sendMessage(Text.of(identity));
+            }
+            else
+            {
+                identity = itemInHand.getItem().getId();
+
+                player.sendMessage(Text.of(identity));
+            }
+        }
 
         return CommandResult.success();
 
@@ -59,3 +60,36 @@ public class Identify implements CommandExecutor
 
 
 }
+
+
+ /* if(src instanceof Player)
+        {
+            Player player = (Player) src;
+            if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent())
+            {
+                String Identity = "";
+                ItemStack itemstack =  player.getItemInHand(HandTypes.MAIN_HAND).get();
+
+                if (itemstack.getItem() instanceof ItemBlock) {
+                    ItemBlock itemBlock = (ItemBlock) itemstack.getItem();
+                    net.minecraft.item.ItemStack nmsStack = (net.minecraft.item.ItemStack) (Object) itemstack;
+                    BlockState blockState = ((BlockState) itemBlock.getBlock().getStateFromMeta(nmsStack.getItemDamage()));
+                    Identity = blockState.getId();
+
+                    player.sendMessage(Text.of(Identity));
+                }
+                else {
+                    Identity = itemstack.getItem().getId();
+
+                    player.sendMessage(Text.of(Identity));
+                }
+
+
+
+            }
+            else
+                src.sendMessage(Text.of(TextColors.RED, "Your Main hand has nothing in it!"));
+        }
+        else
+            throw new CommandException(Text.of("You cannot use this command from console!"));
+*/
